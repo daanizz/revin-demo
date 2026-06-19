575e0609e933298d6f59cce2a721cd1e262e5d17
daanizz <danishvv1011@gmail.com>
2026-06-19 10:47:57 +0530

    minor bug in adding user

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -6,7 +6,7 @@ import {createUser,deleteUserById,fetchUsers,findUserByIdentifier} from "./auth.
 
 export const addUser = async (userData) => {
   const hashPassword=await bcrypt.hash(userData.password,10);
-  userData={...userData,password:hashPassword,phone:phone.replace(/\D/g,"").slice(-10)};
+  userData={...userData,password:hashPassword,phone:userData.phone.replace(/\D/g,"").slice(-10)};
   return createUser(userData);
 };
 
@@ -18,32 +18,6 @@ export const getAllUsers=async()=>{
   return fetchUsers();
 }
 
-// export const loginUserService = async (email, password) => {
-//   const user = await getUser(email);
-
-//   if (!user) {
-//     throw new Error("User not found");
-//   }
-
-//   const isMatch = await bcrypt.compare(password, user.password);
-
-//   if (!isMatch) {
-//     throw new Error("Invalid password");
-//   }
-
-//   const token = jwt.sign(
-//     {
-//       id: user._id,
-//       role: user.role,
-//     },
-//     process.env.JWT_SECRET,
-//     {
-//       expiresIn: "7d",
-//     }
-//   );
-
-//   return {user,token,};
-// };
 
 export const loginUserService = async (identifier,password) => {
   const user = await findUserByIdentifier(identifier);
